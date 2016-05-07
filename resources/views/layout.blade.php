763415325ed8c41f5f3269f114cee4b3fa285a70
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-07 00:45:23 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -30,9 +30,23 @@
                 <a href="#">Contact</a>
             </li>
             @if (Auth::guest())
-                <li><a href="{{ url('#') }}">Login</a></li>
-                <li><a href="{{ url('#') }}">Register</a></li>
+                <li><a href="{{ url('/login') }}">Login</a></li>
+                <li><a href="{{ url('/register') }}">Register</a></li>
             @else
+                <li class="dropdown">
+
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+
+                        {{Auth::user()->name}} <span class="caret"></span>
+                        </a>
+
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="{{url('/logout')}}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+
+
+                    </ul>
+
+                </li>
 
             @endif
         </ul>
