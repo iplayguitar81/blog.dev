8abe55df3836489137a5b9b890de0ac0919af59c
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-07 00:18:29 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Welcome to Bowtiesoft</div>
 
                 <div class="panel-body">
                    Welcome to Bowtiesoft.  This page is under development but should be up and running soon!  Check back soon for updates!
