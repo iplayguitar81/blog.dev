00189781b2bf3d9d04795e429ca7d448293c9450
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-19 14:45:42 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -61,7 +61,7 @@
             @endif
         </ul>
         <a href="#offcanvas" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas></a>
-        <div class="uk-navbar-brand uk-navbar-center uk-visible-small">Brand</div>
+        <div class="uk-navbar-brand uk-navbar-center uk-visible-small"><a class="uk-navbar-brand uk-hidden-small" href="{{url('/')}}"><img src="{{url('/images/bowtiebranded.png')}}" alt="bowtiesoftware &amp; web development"/></a></div>
     </nav>
 
     <div class="uk-grid" data-uk-grid-margin>
@@ -86,10 +86,10 @@
                 <a href="#">Contact</a>
             </li>
             <li>
-                <a href="layouts_contact.html">Contact</a>
+                <a href="#">Contact</a>
             </li>
             <li>
-                <a href="layouts_login.html">Login</a>
+                <a href="#">Login</a>
             </li>
         </ul>
     </div>
