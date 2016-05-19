254ff9e6a476a7f89c31a7d9f1d8a1d45e2cb33e
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-19 00:49:06 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
 
 </div>
 
-<div class="uk-article">
+<div class="uk-container-center">
 
     <div class="uk-width-medium-1-3 uk-row-first">
         <div class="uk-panel"><img src="images/placeholder_400x250.svg" alt="Placeholder"></div>
@@ -26,6 +26,9 @@
     <div class="uk-width-medium-2-3 uk-flex-middle">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
     </div>
+    <hr>
+
+
 </div>
 @endsection
 
