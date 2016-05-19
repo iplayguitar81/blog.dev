b7985cd9235ff16793038f254545c520ec05fa73
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-19 14:31:05 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -25,7 +25,7 @@
 <div class="uk-container uk-container-center uk-margin-top uk-margin-large-bottom">
 
     <nav class="uk-navbar uk-margin-large-bottom">
-        <a class="uk-navbar-brand uk-hidden-small" href="{{url('/')}}">Bowtie Software & Web Development</a>
+        <a class="uk-navbar-brand uk-hidden-small" href="{{url('/')}}">Bowtie Software & Web Development<img src="images/bowtiebranded.png" alt="bowtiesoftware &amp; web development"/></a>
         <ul class="uk-navbar-nav uk-hidden-small">
             <li class="uk-active">
                 <a href="{{url('/')}}" >Home</a>
