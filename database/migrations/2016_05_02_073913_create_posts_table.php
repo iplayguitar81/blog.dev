bdab493f7766caf6887e71f381c55481637fa8a1
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-20 01:38:20 -0700

    get win 10 laravel started yeahhhhhh

--- database/migrations/2016_05_02_073913_create_posts_table.php
+++ database/migrations/2016_05_02_073913_create_posts_table.php
@@ -14,12 +14,18 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function(Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
             $table->string('title');
             $table->string('subHead');
             $table->text('body');
             $table->string('imgPath');
             $table->timestamps();
         });
+        
+        Schema::table('posts', function($table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
     }
 
     /**
