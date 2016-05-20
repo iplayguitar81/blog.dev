5bc61357a1091d255266fa6c36e5ecf6297d406e
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-20 01:30:02 -0700

    get win 10 laravel started yeahhhhhh

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -35,7 +35,8 @@ class PostsController extends Controller
        
         $route->getActionName();
 
-        $posts = Post::paginate(15);
+        $posts = Post::where('user_id','=', Auth::id())->get();
+       # $posts = Post::paginate(15);
 
         return view('posts.index', compact('posts', 'route'));
 
