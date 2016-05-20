904cc8f067afd0fc2df23fd5e77c1ab897bf2c08
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-19 20:42:29 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,7 +66,7 @@
         <p class="uk-article-meta">Written by Author on 12 April 2013. Posted in <a href="#">Blog</a></p>
 
         <p>
-            <a href="layouts_post.html"><img width="900" height="300" class="uk-responsive-width" src="../images/{{ $item->imgPath}}"></a>
+            <a href="layouts_post.html"><img class="uk-responsive-width" src="../images/{{ $item->imgPath}}"></a>
         </p>
 
         <p>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
