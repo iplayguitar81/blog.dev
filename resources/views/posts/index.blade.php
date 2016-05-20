bcfcc52ae8db832a2b5d09bf1c1207435b989b86
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-20 03:17:15 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,10 +2,10 @@
 @section('content')
 <div class="container">
     <h1>Posts @can('isAdmin')<a href="{{ url('/posts/create') }}" class="btn btn-primary pull-right btn-sm">Add New Post</a>@endcan</h1>
-    @foreach($user as $item)
-    <p> User ID:  {{$item->id}} </p>
 
-    @endforeach
+    <p> User ID:  {{$item}} </p>
+
+
     <div class="table">
         <table class="uk-table uk-table-hover uk-table-striped">
             <thead>
