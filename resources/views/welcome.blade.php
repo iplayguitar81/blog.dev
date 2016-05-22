8b5726d1ee6d0016495dc3cf4455822e4e1eda6c
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-21 22:32:20 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,7 +65,7 @@
 
         {{$test_variable = $item->user_id}}
         <p class="uk-article-meta">Written by {{$item->user_id}}
-            {{ Auth::user()->name ::where('id' ==$item->user_id) }}
+            {{ Auth::user()->name->where('id' ==$item->user_id) }}
             on {{ $item->created_at}}.</p>
         <h2>{{ $item->subHead}}</h2>
 
