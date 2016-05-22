2011e875848e89d413b06972114aaab051c1175a
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-21 23:20:54 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
         {{--{{//$users::where('id','like',$item->user_id) -> name()}}--}}
         <p class="uk-article-meta">Written by {{$item->user_id}}
 
-            {{$the_records=array();}}
+            {{$the_records=array()}}
 
             {{--{{$users::find($item->user_id)->name}}--}}
             {{$book = App\User::find($item->user_id)}}
