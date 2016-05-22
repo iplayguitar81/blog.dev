c03de7080c4df00ad72585a5e93865e9d7deafef
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-22 01:35:59 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,23 +3,21 @@
 @section('content')
 <div class="container">
 
-    <h1>Post</h1>
-    <div class="table-responsive">
-        <table class="table table-bordered table-striped table-hover">
-            <thead>
-                <tr>
-                    <th>ID.</th> <th>{{ trans('posts.title') }}</th><th>{{ trans('posts.body') }}</th><th>{{ trans('posts.imgPath') }}</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{{ $post->id }}</td> <td> {{ $post->title }} </td><td> {{ $post->body }} </td><td> <img class="uk-thumbnail uk-align-center" src="../images/{{ $post->imgPath}}"> </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-
-
+    {{--<h1>Post</h1>--}}
+    {{--<div class="table-responsive">--}}
+        {{--<table class="table table-bordered table-striped table-hover">--}}
+            {{--<thead>--}}
+                {{--<tr>--}}
+                    {{--<th>ID.</th> <th>{{ trans('posts.title') }}</th><th>{{ trans('posts.body') }}</th><th>{{ trans('posts.imgPath') }}</th>--}}
+                {{--</tr>--}}
+            {{--</thead>--}}
+            {{--<tbody>--}}
+                {{--<tr>--}}
+                    {{--<td>{{ $post->id }}</td> <td> {{ $post->title }} </td><td> {{ $post->body }} </td><td> <img class="uk-thumbnail uk-align-center" src="../images/{{ $post->imgPath}}"> </td>--}}
+                {{--</tr>--}}
+            {{--</tbody>--}}
+        {{--</table>--}}
+    {{--</div>--}}
 
 
     <br/>
