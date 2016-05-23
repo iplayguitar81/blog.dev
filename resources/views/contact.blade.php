29b398a3c65c6574f6a9623164db9ff3960b22df
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-23 15:48:41 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,8 +8,14 @@
     <form class="uk-form">
         <fieldset>
             <legend>...</legend>
-            <div class="uk-form-row">...</div>
-            <div class="uk-form-row">...</div>
+            <div class="uk-form-row">
+                <input type="text" placeholder=""> <span class="uk-form-help-inline">...</span>
+            </div>
+
+            <div class="uk-form-row">
+                <textarea cols="" rows="" placeholder="">...</textarea>
+                <p class="uk-form-help-block">...</p>
+            </div>
         </fieldset>
     </form>
 
