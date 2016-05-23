27a758dd806e34d317b9339c1394510aa02ee2ec
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-23 13:56:11 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/user_posts.blade.php
+++ resources/views/posts/user_posts.blade.php
@@ -43,7 +43,7 @@
                 @endforeach
                 </tbody>
             </table>
-            {{--<div class="pagination"> {!! $posts->render() !!} </div>--}}
+            <div class="pagination"> {!! $posts->render() !!} </div>
         </div>
 
         {{--@foreach($posts as $item)--}}
