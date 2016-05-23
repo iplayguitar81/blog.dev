5a431498121e76f754d2b2c34bf4c904a84600e8
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-22 21:02:43 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,11 @@
+<style>
+    <?php $url = asset('/fonts/creampuffy.ttf') ?>
+    @font-face {
+        font-family: Creampuff;
+        src: {{$url}};
+    }
+</style>
+
 @extends('layout')
 
 @section('content')
