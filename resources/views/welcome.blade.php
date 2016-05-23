93d9e843c9101b5b5069db6117490954b1a473a9
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-22 21:43:25 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,7 +57,7 @@
 
 
         <h1 class="uk-article-title">
-            <a style="font-family: 'Creampuff' !important;" href="{{ url('posts', $item->id) }}">{{ $item->title }}</a>
+            <a href="{{ url('posts', $item->id) }}">{{ $item->title }}</a>
         </h1>
         <h2>{{ $item->subHead}}</h2>
         {{--{{$posts-> $item->user }}--}}
