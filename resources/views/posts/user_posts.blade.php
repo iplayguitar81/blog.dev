c56219c54220657d8d3d40e3a70343b113a4f9c6
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-22 19:09:50 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/user_posts.blade.php
+++ resources/views/posts/user_posts.blade.php
@@ -11,7 +11,7 @@
             <table class="uk-table uk-table-hover uk-table-striped">
                 <thead>
                 <tr>
-                    <th>{{ trans('posts.title') }}</th><th>{{ trans('posts.subhead') }}</th><th>{{ trans('posts.body') }}</th><th>Image</th>@can('isAdmin')<th>Actions</th>@endcan
+                    <th>{{ trans('posts.title') }}</th><th>{{ trans('posts.subhead') }}</th><th>{{ trans('posts.body') }}</th><th>Image</th><th>Date</th>@can('isAdmin')<th>Actions</th>@endcan
                 </tr>
                 </thead>
                 <tbody>
@@ -23,6 +23,8 @@
                         <td><a href="{{ url('posts', $item->id) }}">{{ $item->title }}</a></td><td>{{ $item->subHead }}</td><td>{{str_limit($item->body, 15)}}</td>
 
                         <td><img class="uk-thumbnail-mini" src="../images/{{ $item->imgPath}}"></td>
+                        <td>{{ $item->created_at}}</td>
+
                         @can('isAdmin')
                             <td>
                                 <a href="{{ url('/posts/' . $item->id . '/edit') }}" class="uk-button uk-button-primary">Update</a>
