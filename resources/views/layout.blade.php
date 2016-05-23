ce316e068d26d547528695315f0c20ebf15a2bcc
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-23 14:09:05 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bowtie Software &amp; Web Development | @yield('title') </title>
-    <link rel="shortcut icon" href="{{ '/images/favicon.ico' }}">
+    <link rel="shortcut icon" href="{{ 'images/favicon.ico' }}">
     <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon.png">
     <link rel="stylesheet" href="/css/uikit.docs.min.css">
     <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
