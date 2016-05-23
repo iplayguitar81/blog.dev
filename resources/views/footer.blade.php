6fb6aeed3c22bae2cfafcab620744a2ddbe248d1
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-23 00:51:35 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,4 @@
 <div class="uk-modal-footer">
-    <div class="uk-align-center">Bowtie Software &amp; Web Development Copyright 2016</div>
+    <div class="uk-align-center">Bowtie Software &amp; Web Development Copyright {{date("Y")}}</div>
 
 </div>
