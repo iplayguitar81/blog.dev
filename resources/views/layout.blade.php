804412224faf26da68a4f6c56d648c5902c91238
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-22 19:50:53 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -134,7 +134,7 @@
 
                 {{--</ul>--}}
 
-                <div class="uk-button-dropdown uk-align-right" data-uk-dropdown="{mode:'click'}" aria-haspopup="true" aria-expanded="false">
+                <div class="uk-button-dropdown uk-align-left" data-uk-dropdown="{mode:'click'}" aria-haspopup="true" aria-expanded="false">
                     <button class="uk-button">{{Auth::user()->name}}&nbsp;&nbsp;<i class="uk-icon-caret-down"></i></button>
                     <div class="uk-dropdown uk-dropdown-bottom" style="top: 30px; left: 0px;">
                         <ul class="uk-nav uk-nav-dropdown">
