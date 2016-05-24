97a8a59e2b1d7a99b5dafdff3973778d98a89492
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-23 18:07:02 -0700

    get win 10 laravel started yeahhhhhh

--- app/Http/Requests/ContactFormRequest.php
+++ app/Http/Requests/ContactFormRequest.php
@@ -25,7 +25,9 @@ class ContactFormRequest extends Request
     public function rules()
     {
         return [
-            //
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required',
         ];
     }
 }
