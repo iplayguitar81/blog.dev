944b68d8191ad45217c6da43c1ac42e8fb5b40b5
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-23 20:24:48 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,12 +52,7 @@
 
 
 <div class="uk-width-medium-3-4 uk-row-first">
-    @section('content')
-        @if(Session::has('message'))
-            <div class="alert alert-info">
-                {{Session::get('message')}}
-            </div>
-        @endif
+
 
     @foreach($posts as $item)
     <article class="uk-article">
