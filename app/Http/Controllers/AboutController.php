f97c8e66685687728aa1da369146c2d12041ff2a
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-23 19:32:59 -0700

    get win 10 laravel started yeahhhhhh

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -19,9 +19,27 @@ class AboutController extends Controller
     {
 
         $this->validate($request, [
-            'title' => 'required',
-            'body' => 'required',
+            'name' => 'required',
+            'email' => 'required',
+            'message' => 'required',
+
         ]);
+
+
+        \Mail::send('emails.contact',
+            array(
+                'name' => $request->get('name'),
+                'email' => $request->get('email'),
+                'user_message' => $request->get('message')
+            ), function($message)
+            {
+                $message->from('info@bowtiesoft.com');
+                $message->to('info@bowtiesoft.com', 'Bowtie Software & Web Development')->subject('TODOParrot Feedback');
+            });
+
+
+
+
         return \Redirect::route('contact')
             ->with('message', 'Thanks for contacting us!');
 
