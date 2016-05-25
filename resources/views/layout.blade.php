d82f261944f2f022f01bc5d629885df11d399fd5
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-25 16:21:32 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -15,6 +15,7 @@
     header#top {
         position:fixed;
     }
+    nav{ position:fixed;}
 </style>
 
  <style>
@@ -39,38 +40,38 @@
 
     <header id="top">
         <a href="{{url('/')}}" ><img alt="bowtie software &amp; web development" src="{{url('/images/bowtiebranded.png')}}"></a>
-
-        <br/>
-        <nav class="uk-navbar uk-margin-large-bottom">
-            {{--<a class="uk-navbar-brand uk-hidden-small" href="{{url('/')}}"><img src="{{url('/images/bowtiebranded.png')}}" alt="bowtiesoftware &amp; web development"/></a>--}}
-            <ul class="uk-navbar-nav uk-hidden-small">
-                <li class="uk-active">
-                    <a href="{{url('/')}}" >Home</a>
-                </li>
-                <li>
-                    <a href="#">Portfolio</a>
-                </li>
-                <li>
-                    <a href="{{url('/posts')}}">Blog</a>
-                </li>
-                <li>
-                    <a href="/contact">Contact</a>
-                </li>
-                @if (Auth::guest())
-                    <li><a href="{{ url('/login') }}">Login</a></li>
-                    {{--<li><a href="{{ url('/register') }}">Register</a></li>--}}
-                @else
+    </header>
+<br/>
+    <nav class="uk-navbar uk-margin-large-bottom">
+        {{--<a class="uk-navbar-brand uk-hidden-small" href="{{url('/')}}"><img src="{{url('/images/bowtiebranded.png')}}" alt="bowtiesoftware &amp; web development"/></a>--}}
+        <ul class="uk-navbar-nav uk-hidden-small">
+            <li class="uk-active">
+                <a href="{{url('/')}}" >Home</a>
+            </li>
+            <li>
+                <a href="#">Portfolio</a>
+            </li>
+            <li>
+                <a href="{{url('/posts')}}">Blog</a>
+            </li>
+            <li>
+                <a href="/contact">Contact</a>
+            </li>
+            @if (Auth::guest())
+                <li><a href="{{ url('/login') }}">Login</a></li>
+                {{--<li><a href="{{ url('/register') }}">Register</a></li>--}}
+            @else
 
 
 
                     {{--<ul href="#" class="uk-nav uk-nav-dropdow" role="button" aria-expanded="false">--}}
 
-                    {{--{{Auth::user()->name}} <span class="caret"></span>--}}
-                    {{--</ul>--}}
+                        {{--{{Auth::user()->name}} <span class="caret"></span>--}}
+                        {{--</ul>--}}
 
-                    {{--<li><a href="{{url('/posts/user_posts')}}"><i class="fa fa-btn fa-sign-out"></i>Posts You Made</a></li>--}}
+                {{--<li><a href="{{url('/posts/user_posts')}}"><i class="fa fa-btn fa-sign-out"></i>Posts You Made</a></li>--}}
 
-                    {{--<li><a href="{{url('/logout')}}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>--}}
+                {{--<li><a href="{{url('/logout')}}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>--}}
 
 
                     {{--<ul class="n" role="menu">--}}
@@ -79,27 +80,23 @@
 
                     {{--</ul>--}}
 
-                    <div class="uk-button-dropdown uk-align-right" data-uk-dropdown="{mode:'click'}" aria-haspopup="true" aria-expanded="false">
-                        <button class="uk-button">{{Auth::user()->name}}&nbsp;&nbsp;<i class="uk-icon-caret-down"></i></button>
-                        <div class="uk-dropdown uk-dropdown-bottom" style="top: 30px; left: 0px;">
-                            <ul class="uk-nav uk-nav-dropdown">
-                                <li><a href="{{url('/posts/user_posts')}}"><i class="fa fa-btn fa-sign-out"></i>Posts You Made</a></li>
-                                <li class="uk-nav-divider"></li>
-                                <li><a href="{{url('/logout')}}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                <div class="uk-button-dropdown uk-align-right" data-uk-dropdown="{mode:'click'}" aria-haspopup="true" aria-expanded="false">
+                    <button class="uk-button">{{Auth::user()->name}}&nbsp;&nbsp;<i class="uk-icon-caret-down"></i></button>
+                    <div class="uk-dropdown uk-dropdown-bottom" style="top: 30px; left: 0px;">
+                        <ul class="uk-nav uk-nav-dropdown">
+                            <li><a href="{{url('/posts/user_posts')}}"><i class="fa fa-btn fa-sign-out"></i>Posts You Made</a></li>
+                            <li class="uk-nav-divider"></li>
+                            <li><a href="{{url('/logout')}}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
 
-                            </ul>
-                        </div>
+                        </ul>
                     </div>
+                </div>
 
-                @endif
-            </ul>
-            <a href="#offcanvas" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas></a>
-            <div class="uk-navbar-brand uk-navbar-center uk-visible-small"><!-- This is where you put branding text --></div>
-        </nav>
-
-
-
-    </header>
+            @endif
+        </ul>
+        <a href="#offcanvas" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas></a>
+        <div class="uk-navbar-brand uk-navbar-center uk-visible-small"><!-- This is where you put branding text --></div>
+    </nav>
 
 
 
