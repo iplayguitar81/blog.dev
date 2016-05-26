df72302b543a623501e0fa918820ad656f121e0e
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-25 23:25:47 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
             on {{ $item->created_at->format('M dS Y') }}</p>
 
         <p>
-            <a href="{{ route('posts.show', [$item->id, str_slug($item->title)]) }}"><img class="uk-responsive-width" src="../images/{{ $item->imgPath}}"></a>
+            <a href="{{ route('posts.show', [$item->id, str_slug($item->title)]) }}"><img class="img-responsive" src="../images/{{ $item->imgPath}}"></a>
         </p>
 
       <p>  {{str_limit($item->body, 100)}}...</p>
