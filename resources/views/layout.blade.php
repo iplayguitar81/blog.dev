3d0098f44a14b0732280653f07291f15712a0963
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-26 00:26:59 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,6 +17,10 @@ ul.pagination{display:inline-block;padding:0;margin:0;font-size:1.2em;color:#E63
     .main-contain{
         margin-top: 5em !important;
     }
+    .main-article-title{
+        color: #E63C4D;
+        text-shadow: silver 2px 1px 2px;
+    }
 
 /* <!-- Way to do it with fonts from dropbox if need be not satisfied with Google fonts.  be ware some fonts are persnickety....--> */
     @font-face {
