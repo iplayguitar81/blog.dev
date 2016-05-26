83042afe42e6f0c159508c9328b32d9e152b308d
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-25 22:46:03 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,7 +17,6 @@
     .main-contain{
         margin-top: 5em !important;
     }
-
     @font-face {
          font-family: 'FabFelt';
          src: url("https://dl.dropboxusercontent.com/s/e0x8cw1pczwvz90/fabfeltscript-bold.woff2"); !important }
@@ -47,13 +46,14 @@
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Page 1</a></li>
-                <li><a href="#">Page 2</a></li>
-                <li><a href="#">Page 3</a></li>
+                <li><a href="{{url('/posts')}}">Blog</a></li>
+                <li><a href="{{url('#')}}">Portfolio</a></li>
+                <li><a href="{{url('/contact')}}">Contact</a></li>
+
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                <li><a href="{{url('/login')}}"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
             </ul>
         </div>
     </div>
