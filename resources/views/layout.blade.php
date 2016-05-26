536e1eb86dbdbd98faed3b4423fc5d780c9d916c
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-25 22:47:59 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -52,7 +52,6 @@
 
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
                 <li><a href="{{url('/login')}}"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
             </ul>
         </div>
