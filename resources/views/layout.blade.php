56c5ac3a9c3b5c8f60be682908daddf6fd69481b
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-26 01:19:49 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -74,6 +74,9 @@ hr {
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="{{url('/login')}}"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                <li><a href="{{url('/posts/user_posts')}}"><i class="fa fa-btn fa-sign-out"></i>Posts You Made</a></li>
+
+                <li><a href="{{url('/logout')}}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
             </ul>
         </div>
     </div>
