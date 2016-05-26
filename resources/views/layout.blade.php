038ca8dc14630a65104f3088b3de1af4a9ec0129
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-25 21:50:06 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -68,35 +68,35 @@
     </div>
 </nav>
 
-<div id="nav-bar-logo-stick" data-uk-sticky="{boundary:'#footsy'}">
-    <header id="top">
-        <a href="{{url('/')}}" ><img alt="bowtie software &amp; web development" src="{{url('/images/bowtiebranded.png')}}"></a>
-    </header>
+{{--<div id="nav-bar-logo-stick" data-uk-sticky="{boundary:'#footsy'}">--}}
+    {{--<header id="top">--}}
+        {{--<a href="{{url('/')}}" ><img alt="bowtie software &amp; web development" src="{{url('/images/bowtiebranded.png')}}"></a>--}}
+    {{--</header>--}}
 
 
 
 
 
-    <br/>
-    <nav class="uk-navbar uk-margin-large-bottom">
+    {{--<br/>--}}
+    {{--<nav class="uk-navbar uk-margin-large-bottom">--}}
         {{--<a class="uk-navbar-brand uk-hidden-small" href="{{url('/')}}"><img src="{{url('/images/bowtiebranded.png')}}" alt="bowtiesoftware &amp; web development"/></a>--}}
-        <ul class="uk-navbar-nav uk-hidden-small">
-            <li class="uk-active">
-                <a href="{{url('/')}}" >Home</a>
-            </li>
-            <li>
-                <a href="#">Portfolio</a>
-            </li>
-            <li>
-                <a href="{{url('/posts')}}">Blog</a>
-            </li>
-            <li>
-                <a href="/contact">Contact</a>
-            </li>
-            @if (Auth::guest())
-                <li><a href="{{ url('/login') }}">Login</a></li>
+        {{--<ul class="uk-navbar-nav uk-hidden-small">--}}
+            {{--<li class="uk-active">--}}
+                {{--<a href="{{url('/')}}" >Home</a>--}}
+            {{--</li>--}}
+            {{--<li>--}}
+                {{--<a href="#">Portfolio</a>--}}
+            {{--</li>--}}
+            {{--<li>--}}
+                {{--<a href="{{url('/posts')}}">Blog</a>--}}
+            {{--</li>--}}
+            {{--<li>--}}
+                {{--<a href="/contact">Contact</a>--}}
+            {{--</li>--}}
+            {{--@if (Auth::guest())--}}
+                {{--<li><a href="{{ url('/login') }}">Login</a></li>--}}
                 {{--<li><a href="{{ url('/register') }}">Register</a></li>--}}
-            @else
+            {{--@else--}}
 
 
 
@@ -116,24 +116,24 @@
 
                 {{--</ul>--}}
 
-                <div class="uk-button-dropdown uk-align-right" data-uk-dropdown="{mode:'click'}" aria-haspopup="true" aria-expanded="false">
-                    <button class="uk-button">{{Auth::user()->name}}&nbsp;&nbsp;<i class="uk-icon-caret-down"></i></button>
-                    <div class="uk-dropdown uk-dropdown-bottom" style="top: 30px; left: 0px;">
-                        <ul class="uk-nav uk-nav-dropdown">
-                            <li><a href="{{url('/posts/user_posts')}}"><i class="fa fa-btn fa-sign-out"></i>Posts You Made</a></li>
-                            <li class="uk-nav-divider"></li>
-                            <li><a href="{{url('/logout')}}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                {{--<div class="uk-button-dropdown uk-align-right" data-uk-dropdown="{mode:'click'}" aria-haspopup="true" aria-expanded="false">--}}
+                    {{--<button class="uk-button">{{Auth::user()->name}}&nbsp;&nbsp;<i class="uk-icon-caret-down"></i></button>--}}
+                    {{--<div class="uk-dropdown uk-dropdown-bottom" style="top: 30px; left: 0px;">--}}
+                        {{--<ul class="uk-nav uk-nav-dropdown">--}}
+                            {{--<li><a href="{{url('/posts/user_posts')}}"><i class="fa fa-btn fa-sign-out"></i>Posts You Made</a></li>--}}
+                            {{--<li class="uk-nav-divider"></li>--}}
+                            {{--<li><a href="{{url('/logout')}}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>--}}
 
-                        </ul>
-                    </div>
-                </div>
+                        {{--</ul>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
 
-            @endif
-        </ul>
-        <a href="#offcanvas" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas></a>
-        <div class="uk-navbar-brand uk-navbar-center uk-visible-small"><!-- This is where you put branding text --></div>
-    </nav>
-</div>
+            {{--@endif--}}
+        {{--</ul>--}}
+        {{--<a href="#offcanvas" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas></a>--}}
+        {{--<div class="uk-navbar-brand uk-navbar-center uk-visible-small"><!-- This is where you put branding text --></div>--}}
+    {{--</nav>--}}
+{{--</div>--}}
 
 
 <div class="uk-container uk-container-center uk-margin-top uk-margin-large-bottom main-contain">
