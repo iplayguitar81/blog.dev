9f82ed52139247d9b004212c9499f835d447be7e
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-26 00:39:08 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -25,6 +25,9 @@ ul.pagination{display:inline-block;padding:0;margin:0;font-size:1.2em;color:#E63
 .main-article-titles a:hover {
     color: #000;
 }
+.article-title-show {
+    color: #E63C4D;
+}
 
 /* <!-- Way to do it with fonts from dropbox if need be not satisfied with Google fonts.  be ware some fonts are persnickety....--> */
     @font-face {
