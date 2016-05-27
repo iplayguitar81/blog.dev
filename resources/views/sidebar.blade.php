87445197a3fb3675b53ba739c303542b876523b3
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-27 01:12:15 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -72,3 +72,11 @@ to Bowtie<br/>Software &amp; Web Development!
         </ul>
 
     </div>
+
+    <div class="list-group">
+        <button type="button" class="list-group-item">Cras justo odio</button>
+        <button type="button" class="list-group-item">Dapibus ac facilisis in</button>
+        <button type="button" class="list-group-item">Morbi leo risus</button>
+        <button type="button" class="list-group-item">Porta ac consectetur ac</button>
+        <button type="button" class="list-group-item">Vestibulum at eros</button>
+    </div>
