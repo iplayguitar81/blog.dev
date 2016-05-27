bb31bfc185b917807e5aaa39e5e7ea920d9b81a6
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-26 17:53:00 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -177,7 +177,7 @@ hr {
     </div>
 <script src="{{url('/js/jquery.js')}}"></script>
 <script src="{{url('/js/bootstrap.min.js')}}"></script>
-<script src="{{url('/js/ckeditor.js')}}"></script>
+
 
 {{--<script>--}}
     {{--(function() {--}}
