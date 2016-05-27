08119b6c8fc5b757c8298946a28a7b18df94a2b4
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-27 02:56:46 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -26,8 +26,9 @@
     <ul class="nav nav-tabs">
         <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
         <li><a data-toggle="tab" href="#menu1"><i class="fa fa-lg fa-facebook"></i>&nbsp;Facebook</a></li>
-        <li><a data-toggle="tab" href="#menu2">Menu 2</a></li>
-        <li><a data-toggle="tab" href="#menu3">Menu 3</a></li>
+        <li><a data-toggle="tab" href="#menu2"><i class="fa fa-lg fa-twitter"></i>&nbsp;Twitter</a></li>
+        <li><a data-toggle="tab" href="#menu3"><i class="fa fa-lg fa-github"></i>&nbsp;GitHub</a></li>
+
     </ul>
 
     <div class="tab-content">
