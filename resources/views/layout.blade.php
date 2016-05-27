c22d2eb8e800552eef0c77f98c7fb656718e091b
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-27 01:07:17 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -67,7 +67,7 @@ iframe{
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{url('/')}}" style="font-family:Pacifico,cursive; font-size: 3em;">
+            <a class="navbar-brand" href="{{url('/')}}" style="font-family:Pacifico,cursive; font-size: 2em;">
                 <img src="{{url('images/bowtie-brand-transparent.png')}}"alt="">
                 <!-- put text here if want/need be-->
 
