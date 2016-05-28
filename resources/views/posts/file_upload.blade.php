6d6d7b946f450712f8cc70ff00648e8c79691434
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-28 02:24:41 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/file_upload.blade.php
+++ resources/views/posts/file_upload.blade.php
@@ -2,6 +2,14 @@
 @section('title', 'Post CSV Upload')
 @section('content')
 
+
+    @if ($errors->any())
+        <ul class="uk-alert-danger">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
   <h1>File Upload...... Coming soon.....</h1>
 
   {!! Form::open(['url' => '/posts', 'class' => '', 'files' =>true]) !!}
