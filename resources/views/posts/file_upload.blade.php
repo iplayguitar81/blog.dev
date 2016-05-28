5c2e1b13955299f588be31aa53f32f4e247e747b
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-28 02:16:08 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/file_upload.blade.php
+++ resources/views/posts/file_upload.blade.php
@@ -4,6 +4,11 @@
 
   <h1>File Upload...... Coming soon.....</h1>
 
+  <div class="form-group">
+      <div class="">
+          <input type="file" name="file" id="file" onchange="readURL(this);"/>
+      </div>
+  </div>
 
 
 @endsection
