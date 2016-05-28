59c79870e200055cb6bc6f851efcabaf32e87f2f
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-28 02:57:52 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/file_upload.blade.php
+++ resources/views/posts/file_upload.blade.php
@@ -18,10 +18,10 @@
   <h1>File Upload...... Coming soon.....</h1>
 
   {!! Form::open(['url' => '/posts', 'class' => '', 'files' =>true]) !!}
-      <div class="">
+
           <input type="file" name="csv-file" id="csv-file"/>
-      </div>
-  </div>
+
+
 
   {{csrf_field()}}
     <br/>
