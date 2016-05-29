60a99a8b0e8549d826f157613dd5a06d1f732acb
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-28 21:30:46 -0700

    get win 10 laravel started yeahhhhhh

--- database/migrations/2016_05_02_073913_create_posts_table.php
+++ database/migrations/2016_05_02_073913_create_posts_table.php
@@ -20,6 +20,7 @@ class CreatePostsTable extends Migration
             $table->text('body');
             $table->string('imgPath');
             $table->timestamps();
+            
         });
 
         Schema::table('posts', function($table) {
