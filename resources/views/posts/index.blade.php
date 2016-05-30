96e2b3fccdcd3d6bb0a5c36b85d266144cd5de65
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-29 22:22:57 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,7 +12,19 @@
     {{--<p>{{$tubular->imgpath}}</p>--}}
     {{--@endforeach--}}
 
-    @can('isAdmin') <a href="{{ url('/posts/create') }}" class="btn btn-primary pull-right btn-sm">Add New Post</a><br/><a href="{{ url('/posts/file_upload') }}" class="btn btn-success pull-right btn-sm">Import CSV Posts</a>@endcan
+    @can('isAdmin')
+
+        <div class="panel panel-success"> <div class="panel-heading">
+                <h3 class="panel-title">Nifty Admin Functions</h3> </div>
+            <div class="panel-body"> Panel content
+
+
+            </div> </div>
+
+        <a href="{{ url('/posts/create') }}" class="btn btn-primary pull-right btn-sm">Add New Post</a><br/><a href="{{ url('/posts/file_upload') }}" class="btn btn-success pull-right btn-sm">Import CSV Posts</a>
+
+
+    @endcan
 
 
 
