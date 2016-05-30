cc2e3fd5d15b17069a9bbcd2fb3fce1000b12bad
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-29 22:24:25 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,13 +14,13 @@
 
     @can('isAdmin')
 
-        <div class="panel panel-success"> <div class="panel-heading">
+        <div class="panel panel-success pull-right"> <div class="panel-heading">
                 <h3 class="panel-title">Nifty Admin Functions</h3> </div>
             <div class="panel-body">
 
-                <a href="{{ url('/posts/create') }}" class="btn btn-primary pull-right btn-sm">Add New Post</a>
+                <a href="{{ url('/posts/create') }}" class="btn btn-primary btn-sm">Add New Post</a>
                 <br/>
-                <a href="{{ url('/posts/file_upload') }}" class="btn btn-success pull-right btn-sm">Import CSV Posts</a>
+                <a href="{{ url('/posts/file_upload') }}" class="btn btn-success btn-sm">Import CSV Posts</a>
 
 
 
