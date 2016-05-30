c72b33e86c1b9a134e2a5db58b4d4fe4d38069f0
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-29 19:45:25 -0700

    get win 10 laravel started yeahhhhhh

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -120,7 +120,7 @@ class PostsController extends Controller
 
                        // Post::create($row);
 
-                        $csv_import = new Post(['user_id'=> Auth::user()->id,'title' => 'title','subhead' => 'subhead','body' => 'body','imgpath' => 'imgpath' ]);
+                        $csv_import = new Post(['user_id'=> Auth::user()->id,'title' => $row->title,'subhead' => $row->subhead,'body' => $row->body,'imgpath' => $row->imgpath ]);
                         $csv_import->save();
 
 
