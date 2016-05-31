bf23a5850374d663729015bdb4b356347d9f2a11
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-31 00:40:31 -0700

    get win 10 laravel started yeahhhhhh

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -92,19 +92,13 @@ class PostsController extends Controller
 
     public function userRating(){
 
-        $post = \Post::first();
-
-        $rating = new \Rating;
-        $rating->rating = 5;
-        $rating->user_id = \Auth::id();
-
-        $post->ratings()->save($rating);
-
-       dd(\Post::first()->ratings);
-
-      //  return view("posts.post_rating");
-
-
+//        $post = \Post::first();
+//        $rating = new \Rating;
+//        $rating->rating = 5;
+//        $rating->user_id = \Auth::id();
+//        $post->ratings()->save($rating);
+//       dd(\Post::first()->ratings);
+//      //  return view("posts.post_rating");
 
     }
 
