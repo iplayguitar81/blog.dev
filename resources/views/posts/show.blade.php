0c86027414e7d432e1c14646fffcb33922959dc4
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-31 00:41:40 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,12 @@
 @extends('layout')
-
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=409035349261019";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 @section('title', $post->title)
 @section('content')
 
@@ -37,6 +44,10 @@
 
     <br/>
 
+        <div class="fb-comments" data-href="https://www.bowtiesoft.com/posts/32/drexler-vs-jordan" data-numposts="10"></div>
+
+    <br/>
+
 
 
     <a href="{{url('posts')}}">
@@ -50,6 +61,9 @@
     </a>
 
     </div>
+
+
+
     @endsection
 
 
