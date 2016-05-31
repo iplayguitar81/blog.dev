367c03e01a9bfa363239155aa47b8cd363745e34
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-31 00:01:15 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/post_rating.blade.php
+++ resources/views/posts/post_rating.blade.php
@@ -1,17 +1,17 @@
-{!! Form::open(['url' => '/posts', 'class' => 'form-horizontal']) !!}
+{{--{!! Form::open(['url' => '/posts', 'class' => 'form-horizontal']) !!}--}}
 
-<div class="form-group {{ $errors->has('rating') ? 'has-error' : ''}}">
-    {!! Form::label('rating', trans('ratings.rating'), ['class' => 'col-sm-3 control-label']) !!}
-    <div class="col-sm-6">
-        {!! Form::number('rating', null, ['class' => 'form-control']) !!}
-        {!! $errors->first('rating', '<p class="help-block">:message</p>') !!}
-    </div>
-</div>
+{{--<div class="form-group {{ $errors->has('rating') ? 'has-error' : ''}}">--}}
+    {{--{!! Form::label('rating', trans('ratings.rating'), ['class' => 'col-sm-3 control-label']) !!}--}}
+    {{--<div class="col-sm-6">--}}
+        {{--{!! Form::number('rating', null, ['class' => 'form-control']) !!}--}}
+        {{--{!! $errors->first('rating', '<p class="help-block">:message</p>') !!}--}}
+    {{--</div>--}}
+{{--</div>--}}
 
 
-<div class="form-group">
-    <div class="col-sm-offset-3 col-sm-3">
-        {!! Form::submit('Create', ['class' => 'btn btn-primary form-control']) !!}
-    </div>
-</div>
-{!! Form::close() !!}
+{{--<div class="form-group">--}}
+    {{--<div class="col-sm-offset-3 col-sm-3">--}}
+        {{--{!! Form::submit('Create', ['class' => 'btn btn-primary form-control']) !!}--}}
+    {{--</div>--}}
+{{--</div>--}}
+{{--{!! Form::close() !!}--}}
