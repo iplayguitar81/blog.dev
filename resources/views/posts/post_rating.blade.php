42f431c73afa191c14762c503f125f2f586eea7b
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-30 21:46:37 -0700

    get win 10 laravel started yeahhhhhh

---
+++ resources/views/posts/post_rating.blade.php
@@ -0,0 +1,25 @@
+{!! Form::open(['url' => '/posts', 'class' => 'form-horizontal']) !!}
+
+<div class="form-group {{ $errors->has('contents') ? 'has-error' : ''}}">
+    {!! Form::label('contents', trans('ratings.contents'), ['class' => 'col-sm-3 control-label']) !!}
+    <div class="col-sm-6">
+        {!! Form::textarea('contents', null, ['class' => 'form-control']) !!}
+        {!! $errors->first('contents', '<p class="help-block">:message</p>') !!}
+    </div>
+</div>
+
+<div class="form-group {{ $errors->has('rating') ? 'has-error' : ''}}">
+    {!! Form::label('rating', trans('ratings.rating'), ['class' => 'col-sm-3 control-label']) !!}
+    <div class="col-sm-6">
+        {!! Form::number('rating', null, ['class' => 'form-control']) !!}
+        {!! $errors->first('rating', '<p class="help-block">:message</p>') !!}
+    </div>
+</div>
+
+
+<div class="form-group">
+    <div class="col-sm-offset-3 col-sm-3">
+        {!! Form::submit('Create', ['class' => 'btn btn-primary form-control']) !!}
+    </div>
+</div>
+{!! Form::close() !!}
