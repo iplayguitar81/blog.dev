3af498e6d24aec607035034f9d5b6fa265fabea8
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-31 03:56:28 -0700

    get win 10 laravel started yeahhhhhh

---
+++ database/migrations/2016_05_31_034447_create_ratings_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('ratings', function ($table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('rating');
+            $table->integer('user_id')->unsigned();
+            $table->index('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::drop('ratings');
+    }
+}
