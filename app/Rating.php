79c8cb6ed5596ceb13d1319dc8bbcd0bb13a6613
Chris M Snyder <iplayguitar81@yahoo.com>
2016-05-30 18:47:51 -0700

    get win 10 laravel started yeahhhhhh

---
+++ app/Rating.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rating extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'ratings';
+
+    /**
+    * The database primary key value.
+    *
+    * @var string
+    */
+    protected $primaryKey = 'id';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['rating_id', 'post_id', 'contents', 'rater_email', 'rating'];
+}
