e12ba4a77b2bb1fbc201dcd8defc3a2a06411def
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-02 01:04:14 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -9,7 +9,7 @@
 
                 {{Auth::user()->name}}
 
-                @if(is_null(Auth::user()->avatar || isset(Auth::user()->avatar)))
+                @if(is_null(Auth::user()->avatar || !isset(Auth::user()->avatar)))
 
                 @else
 
