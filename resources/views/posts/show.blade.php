e6adcbc104fcc5bf4cef43fcf1d58c433913b6b4
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-03 00:28:34 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -61,7 +61,7 @@
 
 
 
-                    <div class="col-md-3"><? $avatar = App\User::find($rating->user_id)->avatar; ?>
+                    <div class="col-md-2 offset-1"><? $avatar = App\User::find($rating->user_id)->avatar; ?>
 
                         @if(empty($avatar))
                            <p>No Avatar Image</p>
