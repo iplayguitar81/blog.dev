a64ea0c4bae3fa54f08d8346c635db925297930f
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-03 00:13:00 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -64,23 +64,23 @@
                     <div class="col-md-3"><? $avatar = App\User::find($rating->user_id)->avatar; ?>
 
                         @if(empty($avatar))
-
                             No Avatar Image
                         @else
 
                             <img src="{{$avatar}}" class="img-circle avatar" alt="user profile image">
 
                         @endif
+                        <a href="#"><b>{{$author}}</b></a>rated this post.
+
+
+                        <span class="text-muted time">{{$rating->created_at->format('M dS Y')}}</span>
 
                     </div>
 
                     <div class="col-md-6"><p>{{$rating->rate_message}}</p></div>
                     <div class="col-md-3">
-                        <div class="title h5">
-                            <a href="#"><b>{{$author}}</b></a>rated this post.
-                        </div>
 
-                        <span class="text-muted time">{{$rating->created_at->format('M dS Y')}}</span>
+
                         <div class="pull-right">
                             <p>{{$rating->rating}}/5 Stars</p>
                             <div class="rating "><div class="stars"></div><div class="back" style="width:{{$rate_pct_reviewer}}%;"></div></div>
