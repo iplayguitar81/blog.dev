d3794362cef5defefe59735018ee009e530d589f
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-04 03:06:16 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show_user.blade.php
+++ resources/views/posts/show_user.blade.php
@@ -13,12 +13,14 @@
 
 <p><img src="{{$user->avatar}}" alt="user avatar"></p>
 <p>Member since: {{$user->created_at->format('M jS, Y')}}</p>
-
+     <? $title = App\Post::find($rating->post_id)->title; ?>
      <p>{{$ratings}}</p>
 
      @foreach($ratings as $rating)
 {{--<span>test this line out below to be sure output is for correct user.....</span>--}}
          {{--<p>{{$rating->user_id}}</p>--}}
+
+     <p>{{$rating->post_id}}</p>
          <p>Rating Comments: {{$rating->rate_message}}</p>
          <p>{{$rating->rate_message}}</p>
 
