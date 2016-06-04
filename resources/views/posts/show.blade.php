3a3d13416957fff2548df036b7808e4aea4eb89a
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-04 02:23:20 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -65,7 +65,7 @@
         @endif
             <? $author = App\User::find($rating->user_id)->name; ?>
 
-               <a href="{{ route('posts.show_user', [$rating->user_id, str_slug($author)])}}">Test This Route Link to User......</a>
+               <a href="{{ route('posts.show_user')}}">Test This Route Link to User......</a>
 
 
 
