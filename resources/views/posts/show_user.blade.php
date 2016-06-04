d22056ea011020b23c7b41076819e8256d19e1e8
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-04 02:42:41 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show_user.blade.php
+++ resources/views/posts/show_user.blade.php
@@ -4,10 +4,14 @@
 
 
 @section('content')
+
+ <div class="row">
+ <div class="col-md-12">
+
 <h1>{{$user->name}}</h1>
 
 
-<img src="{{$user->avatar}}" alt="user avatar">
-
-
+<p><img src="{{$user->avatar}}" alt="user avatar"></p>
+</div>
+</div>
     @endsection
