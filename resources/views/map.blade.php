b22142345ec07eafeba0c16a11c739c97ed86238
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-05 02:36:50 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -11,3 +11,8 @@
 
 
     @endsection
+<link rel="stylesheet" href="//code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.css">
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script type="text/javascript" src="http://www.google.com/jsapi"></script>
+<script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=false&amp;v=3&amp;libraries=geometry"></script>
+<script type="text/javascript" src="http://geoxml3.googlecode.com/svn/branches/polys/geoxml3.js"></script>
