971672d4baa79fbc18099c12a7fdf846de96f1fe
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-04 22:40:04 -0700

    get win 10 laravel started yeahhhhhh

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,7 +18,9 @@ use AdamWathan\EloquentOAuth\Facades\OAuth;
 //use Stevebauman\Location\Facades\Location;
 //use Stevebauman\Location\Objects\Location;
 //use Stevebauman\Location;
-use Stevebauman\Location\Location;
+//use Stevebauman\Location\Location;
+
+use Stevebauman\Location\Objects\Location;
 
 //use GeoIp2\Record\Location;
 
