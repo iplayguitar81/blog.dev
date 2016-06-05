de2055ca7c51ad55931924a8bda4bd405e80fc79
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-05 02:48:54 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -12,10 +12,12 @@
     </div>
     <br/>
 
+    <div id="wrappa">
         <div id="loading_animation"><h3 class="contact_header">Finding locations near you...</h3><img src="loader.gif" alt="loading location"/> <img src="loader.gif" alt="loading location"/><img src="loader.gif" alt="loading location"/></div>
         <div id="map_canvas"></div>
         <br/>
         <div id="sidebar" data-role="collapsible">
+            </div>
         </div>
         <br/>
         <hr>
