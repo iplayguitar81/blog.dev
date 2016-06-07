1ad976270a9cc110a30797dc54aff61d9fa17595
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-07 00:58:31 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -366,7 +366,7 @@
 
             })
 
-            codeAddress2({!! json_encode($location['city']) !!})
+            codeAddress2({{$location['city']}})
 
         });
         $("#form1").validate({
