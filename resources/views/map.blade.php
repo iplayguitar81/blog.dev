845927f1dd491ea9568d61aa815e8ef5fd6695c7
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-07 01:46:52 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -340,6 +340,14 @@
         $(document).ready(function () {
             $("#search_holder").hide();
             $("h1#locations_header").hide();
+
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(handle_geolocation_query, handle_errors)
+
+
+            } else {
+                alert("Device probably not ready.")
+            }
             var healCodeLoadingInterval = setInterval(function(){
                 var healCodeLoading = document.getElementById("address2").value;
 
@@ -357,13 +365,7 @@
                 }
 
             }, 100);
-//            if (navigator.geolocation) {
-//                navigator.geolocation.getCurrentPosition(handle_geolocation_query, handle_errors)
-//
-//
-//            } else {
-//                alert("Device probably not ready.")
-//            }
+
         });
         var FT_TableID = "1hFcGeKfBgELMvxajsoMvgabWJX9jwNDKUB4HoAxG";
         google.load("visualization", "1", {packages: ["corechart", "table", "geomap"]});
