e0a8830d2c2a1d2612217f82f8c56c4f6311fc22
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-08 02:15:57 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/search.blade.php
+++ resources/views/posts/search.blade.php
@@ -12,6 +12,14 @@
     @foreach($results2 as $result)
     <article>
         <h3><a href="{{ route('posts.show', [$result->id, str_slug($result->title)]) }}">{{$result->title}}</a></h3>
+        <p>Written by:
+            @if($item->user_id != null)
+                <? $author = App\User::find($item->user_id)->name; ?>
+
+                {{$author}}
+            @endif
+        </p>
+        <br/>
         <span class="text-lowercase">{{$result->created_at->format('M dS, Y')}}</span>
         <p>{{$result->user_id}}</p>
         <p>{{$result->body}}</p>
