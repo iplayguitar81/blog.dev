3072955641839a4843837b8161f9bb60780c808f
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-08 00:42:41 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/search.blade.php
+++ resources/views/posts/search.blade.php
@@ -13,7 +13,7 @@
     <article>
 
         <h3>{{$result->title}}</h3>
-        <span class="text-lowercase">{{$result->created_at->format('m-dS-Y')}}</span>
+        <span class="text-lowercase">{{$result->created_at->format('M dS, Y')}}</span>
 
         <p>{{$result->user_id}}</p>
         <p>{{$result->body}}</p>
