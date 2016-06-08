3f04c57ff93b5757f25694171a7f678fd6a14f35
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-07 23:10:46 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -298,16 +298,7 @@ p.article-texterson:first-letter{color:#E63C4D;font-size:1.7em;text-shadow:1px 1
             </ul>
 
 
-<div class="center-block">
-    {!! Form::open(array('route' => 'posts.search', 'class'=>'form navbar-form navbar-right searchform')) !!}
-    {!! Form::text('search', null,
-                           array('required',
-                                'class'=>'form-control',
-                                'placeholder'=>'Search for a tutorial...')) !!}
-    {!! Form::submit('Search',
-                               array('class'=>'btn btn-default')) !!}
-    {!! Form::close() !!}
-</div>
+
 
             <ul class="nav navbar-nav navbar-right">
                 @if (Auth::guest())
