f0a244192c710c255aabc92535f151be1a9898c1
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-08 00:47:47 -0700

    get win 10 laravel started yeahhhhhh

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -367,7 +367,7 @@ class PostsController extends Controller
                 ->orWhere('body', 'like', "%{$user_input}%")
                 ->orWhere('subhead', 'like', "%{$user_input}%");
 
-        })->get();
+        })->paginate(3);
 
         return View('posts.search', compact('results', 'user_input'));
 
