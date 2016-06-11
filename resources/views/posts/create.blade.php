d7268c3e7c0ba5147234994e3236b0ba4f66875d
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-11 00:11:30 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -105,6 +105,10 @@
         var myDropzone = new Dropzone("div#dropzoneFileUpload", { url: "/posts/create"});
         // other code here
 
+        params: {
+            _token: token
+        }
+
     }
 
 
