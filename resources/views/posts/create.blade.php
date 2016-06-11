4b6f2fea44d23804e06f2ef3f5519c7b6f656208
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-10 23:43:41 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -95,4 +95,24 @@
     tinymce.init({ selector:'textarea',plugins: "media" });
 </script>
 
+<script type="text/javascript">
+    var baseUrl = "{{ url('/') }}";
+    var token = "{{ Session::getToken() }}";
+    Dropzone.autoDiscover = false;
+    var myDropzone = new Dropzone("div#dropzoneFileUpload", {
+        url: baseUrl + "/dropzone/uploadFiles",
+        params: {
+            _token: token
+        }
+    });
+    Dropzone.options.myAwesomeDropzone = {
+        paramName: "file", // The name that will be used to transfer the file
+        maxFilesize: 2, // MB
+        addRemoveLinks: true,
+        accept: function(file, done) {
+
+        },
+    };
+</script>
+
 
