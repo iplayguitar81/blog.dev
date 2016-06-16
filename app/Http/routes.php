b14c425ceade3d90313a54c60ca281734d5fbfaf
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-15 22:29:18 -0700

    get win 10 laravel started yeahhhhhh

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -161,7 +161,7 @@ Route::get('/home', 'HomeController@index');
 
 Route::get('/posts/create', ['as' => 'upload', 'uses' => 'PostsController@getUpload']);
 Route::post('upload', ['as' => 'posts/create', 'uses' =>'PostsController@postUpload']);
-Route::post('upload/delete', ['as' => 'posts/create', 'uses' =>'PostsController@deleteUpload']);
+Route::post('upload/delete', ['as' => 'posts/image/delete', 'uses' =>'PostsController@deleteUpload']);
 
 
 //Route::post(
