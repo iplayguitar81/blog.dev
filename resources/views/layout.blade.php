518fa46d9fde9801a7242a0147c01a73d54171e2
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-02 00:41:43 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -834,6 +834,8 @@ p.article-texterson:first-letter{color:#E63C4D;font-size:1.7em;text-shadow:1px 1
     @import "https://fonts.googleapis.com/css?family=Boogaloo";
     @import "https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.3.0/min/dropzone.min.css";
     @import "{{url('/css/default-skin.css')}}";
+    @import "{{url('/css/photoswipe.css')}}";
+
 
     @import "//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css";
     @import "//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css";
