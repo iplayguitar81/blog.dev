8b1c5ac3f0829f7d32e2cbbf759b3946f2c90636
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-02 15:47:16 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -74,7 +74,7 @@
                             <div class="row">
 
 
-                                <div class="col-lg-1">
+                                <div class="col-lg-12">
 
 
                                     <div class="my-gallery" itemscope itemtype="http://schema.org/ImageGallery">
