19faa614766266e62da72e2f33c411ffad65abce
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-03 16:02:59 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/test_code.blade.php
+++ resources/views/posts/test_code.blade.php
@@ -2,20 +2,20 @@
 
 @section('content')
 
-{{$post}}
-    <div class="row">
+{{--{{$post}}--}}
+    {{--<div class="row">--}}
 
 
-            <div class="col-md-12">
+            {{--<div class="col-md-12">--}}
 
-                <div id="carousel-id" class="carousel slide" data-ride="carousel">
+                {{--<div id="carousel-id" class="carousel slide" data-ride="carousel">--}}
         {{--<ol class="carousel-indicators">--}}
             {{--<li data-target="#carousel-id" data-slide-to="0" class=""></li>--}}
             {{--<li data-target="#carousel-id" data-slide-to="1" class=""></li>--}}
             {{--<li data-target="#carousel-id" data-slide-to="2" class="active"></li>--}}
         {{--</ol>--}}
-        <div class="carousel-inner" style="height: 300px;">
-            @foreach($post->images as $image)
+        {{--<div class="carousel-inner" style="height: 300px;">--}}
+            {{--@foreach($post->images as $image)--}}
 
                 {{--*/ @ $pathy =$image->file_path  /*--}}
 
@@ -25,8 +25,8 @@
 
 
 
-            <div class="item">
-                <img  src="{{url($image->file_path)}}"  alt="" />
+            {{--<div class="item">--}}
+                {{--<img  src="{{url($image->file_path)}}"  alt="" />--}}
                 {{--<div class="container">--}}
                     {{--<div class="carousel-caption">--}}
                         {{--<h1>Example headline.</h1>--}}
@@ -35,8 +35,8 @@
                         {{--</p>--}}
                     {{--</div>--}}
                 {{--</div>--}}
-            </div>
-            @endforeach
+            {{--</div>--}}
+            {{--@endforeach--}}
 
             {{--<div class="item">--}}
                 {{--<img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzY2NiI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNmE2YTZhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+U2Vjb25kIHNsaWRlPC90ZXh0Pjwvc3ZnPg==">--}}
@@ -60,16 +60,16 @@
                     {{--</div>--}}
                 {{--</div>--}}
             {{--</div>--}}
-        </div>
-        <a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
-        <a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
-    </div>
-</div>
+        {{--</div>--}}
+        {{--<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>--}}
+        {{--<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>--}}
+    {{--</div>--}}
+{{--</div>--}}
 
-        </div>
+        {{--</div>--}}
 
 
-    @endsection
+
 
 
 <div id="portfolio">
@@ -184,17 +184,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 {{--<div id="myCarousel" class="carousel slide" data-ride="carousel">--}}
     {{--<!-- Indicators -->--}}
     {{--<ol class="carousel-indicators">--}}
@@ -250,6 +239,10 @@
 </div>
         </div>
     </div>
+
+@endsection
+
+
 <script>
     // initialize with defaults
 
