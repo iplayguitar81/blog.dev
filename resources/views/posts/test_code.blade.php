3fef9293f8879b4b3527c24cdcd5ba64b0f27628
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-02 22:33:42 -0700

    get win 10 laravel started yeahhhhhh

---
+++ resources/views/posts/test_code.blade.php
@@ -0,0 +1,118 @@
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="carousel slide" id="myCarousel">
+            <div class="carousel-inner">
+                <div class="item active">
+                    <ul class="thumbnails">
+                        <li class="span3">
+                            <div class="caption">
+                                <h5>Caption</h5>
+                            </div>
+                            <div class="thumbnail">
+                                <img src="http://placehold.it/260x180" alt="">
+                            </div>
+                        </li>
+                        <li class="span3">
+                            <div class="caption">
+                                <h5>Caption</h5>
+                            </div>
+                            <div class="thumbnail">
+                                <img src="http://placehold.it/260x180" alt="">
+                            </div>
+                        </li>
+                        <li class="span3">
+                            <div class="caption">
+                                <h5>Caption</h5>
+                            </div>
+                            <div class="thumbnail">
+                                <img src="http://placehold.it/260x180" alt="">
+                            </div>
+                        </li>
+                        <li class="span3">
+                            <div class="caption">
+                                <h5>Caption</h5>
+                            </div>
+                            <div class="thumbnail">
+                                <img src="http://placehold.it/260x180" alt="">
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="item">
+                    <ul class="thumbnails">
+                        <li class="span3">
+                            <div class="caption">
+                                <h5>Caption</h5>
+                            </div>
+                            <div class="thumbnail">
+                                <img src="http://placehold.it/260x180" alt="">
+                            </div>
+                        </li>
+                        <li class="span3">
+                            <div class="caption">
+                                <h5>Caption</h5>
+                            </div>
+                            <div class="thumbnail">
+                                <img src="http://placehold.it/260x180" alt="">
+                            </div>
+                        </li>
+                        <li class="span3">
+                            <div class="caption">
+                                <h5>Caption</h5>
+                            </div>
+                            <div class="thumbnail">
+                                <img src="http://placehold.it/260x180" alt="">
+                            </div>
+                        </li>
+                        <li class="span3">
+                            <div class="caption">
+                                <h5>Caption</h5>
+                            </div>
+                            <div class="thumbnail">
+                                <img src="http://placehold.it/260x180" alt="">
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="item">
+                    <ul class="thumbnails">
+                        <li class="span3">
+                            <div class="caption">
+                                <h5>Caption</h5>
+                            </div>
+                            <div class="thumbnail">
+                                <img src="http://placehold.it/260x180" alt="">
+                            </div>
+                        </li>
+                        <li class="span3">
+                            <div class="caption">
+                                <h5>Caption</h5>
+                            </div>
+                            <div class="thumbnail">
+                                <img src="http://placehold.it/260x180" alt="">
+                            </div>
+                        </li>
+                        <li class="span3">
+                            <div class="caption">
+                                <h5>Caption</h5>
+                            </div>
+                            <div class="thumbnail">
+                                <img src="http://placehold.it/260x180" alt="">
+                            </div>
+                        </li>
+                        <li class="span3">
+                            <div class="caption">
+                                <h5>Caption</h5>
+                            </div>
+                            <div class="thumbnail">
+                                <img src="http://placehold.it/260x180" alt="">
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <a data-slide="prev" href="#myCarousel" class="left carousel-control">‹</a>
+            <a data-slide="next" href="#myCarousel" class="right carousel-control">›</a>
+        </div>
+    </div>
+</diV>
