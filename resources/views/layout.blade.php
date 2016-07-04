3069b24e7f131f5a3cdb1687b5502ad38d292f2a
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-03 22:43:58 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -878,7 +878,7 @@ p.article-texterson:first-letter{color:#E63C4D;font-size:1.7em;text-shadow:1px 1
     }
     #owl-demo .item img{
         display: block;
-        width: 100%;
+        width: 234px;
         height: auto;
     }
 
