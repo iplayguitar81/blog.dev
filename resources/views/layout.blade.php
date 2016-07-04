3cd472f13ddf206ed9f77534e7ae838301c77cb2
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-03 17:53:35 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -869,7 +869,6 @@ p.article-texterson:first-letter{color:#E63C4D;font-size:1.7em;text-shadow:1px 1
             speed: 500,
             fade: true,
             cssEase: 'linear',
-            centerMode: true,
             slidesToShow: 3,
             slidesToScroll: 3
     });
