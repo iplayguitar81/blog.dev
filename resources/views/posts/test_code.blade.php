2569fc219dca41d830b833df923f89831d778a26
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-04 01:58:42 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/test_code.blade.php
+++ resources/views/posts/test_code.blade.php
@@ -154,7 +154,7 @@
         {{--*/ @ $dimensions =$width.'x'.$height  /*--}}
 
     {{--<div class="item" style="">--}}
-        <figure class="item" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+        <figure class="item col-md-4" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
             <a href="{{url($image->file_path)}}" itemprop="contentUrl" data-size="{{$dimensions}}">
             <img class="thumbnail img-responsive" src="{{url($image->file_path)}}"  alt=""/>
                 </a>
