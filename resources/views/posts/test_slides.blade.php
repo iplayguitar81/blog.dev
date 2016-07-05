d26c4b2956bc316da604d3457183d7fbf71d5046
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-05 13:41:58 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/test_slides.blade.php
+++ resources/views/posts/test_slides.blade.php
@@ -6,29 +6,29 @@
 
 
     <div class="container">
-        <h1>OwlCarousel & PhotoSwipe</h1>
+        <h1>OwlCarousel &amp; PhotoSwipe</h1>
 
         <h2>First gallery</h2>
         <ul class="owl-carousel">
-            <li><a href="./images/photo01_lg.jpg" data-size="960x640"><img src="./images/photo01.jpg" alt="写真1"></a></li>
-            <li><a href="./images/photo02_lg.jpg" data-size="960x640"><img src="./images/photo02.jpg" alt="写真2"></a></li>
-            <li><a href="./images/photo03_lg.jpg" data-size="960x640"><img src="./images/photo03.jpg" alt="写真3"></a></li>
-            <li><a href="./images/photo04_lg.jpg" data-size="960x640"><img src="./images/photo04.jpg" alt="写真4"></a></li>
-            <li><a href="./images/photo05_lg.jpg" data-size="960x640"><img src="./images/photo05.jpg" alt="写真5"></a></li>
-            <li><a href="./images/photo06_lg.jpg" data-size="960x640"><img src="./images/photo06.jpg" alt="写真6"></a></li>
-            <li><a href="./images/photo07_lg.jpg" data-size="960x640"><img src="./images/photo07.jpg" alt="写真7"></a></li>
-            <li><a href="./images/photo08_lg.jpg" data-size="960x640"><img src="./images/photo08.jpg" alt="写真8"></a></li>
-            <li><a href="./images/photo09_lg.jpg" data-size="960x640"><img src="./images/photo09.jpg" alt="写真9"></a></li>
+            <li><a href="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo01_lg.jpg" data-size="960x640"><img src="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo01.jpg" alt="1"></a></li>
+            <li><a href="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo02_lg.jpg" data-size="960x640"><img src="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo02.jpg" alt="2"></a></li>
+            <li><a href="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo03_lg.jpg" data-size="960x640"><img src="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo03.jpg" alt="3"></a></li>
+            <li><a href="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo04_lg.jpg" data-size="960x640"><img src="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo04.jpg" alt="4"></a></li>
+            <li><a href="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo05_lg.jpg" data-size="960x640"><img src="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo05.jpg" alt="5"></a></li>
+            <li><a href="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo06_lg.jpg" data-size="960x640"><img src="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo06.jpg" alt="6"></a></li>
+            <li><a href="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo07_lg.jpg" data-size="960x640"><img src="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo07.jpg" alt="7"></a></li>
+            <li><a href="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo08_lg.jpg" data-size="960x640"><img src="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo08.jpg" alt="8"></a></li>
+            <li><a href="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo09_lg.jpg" data-size="960x640"><img src="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo09.jpg" alt="9"></a></li>
         </ul>
 
         <h2>Second gallery</h2>
         <ul class="owl-carousel">
-            <li><a href="./images/photo10_lg.jpg" data-size="960x640" data-title="タイトル1"><img src="./images/photo10.jpg" alt="写真10"></a></li>
-            <li><a href="./images/photo11_lg.jpg" data-size="960x640" data-title="タイトル2"><img src="./images/photo11.jpg" alt="写真11"></a></li>
-            <li><a href="./images/photo12_lg.jpg" data-size="960x640" data-title="タイトル3"><img src="./images/photo12.jpg" alt="写真12"></a></li>
-            <li><a href="./images/photo13_lg.jpg" data-size="960x640" data-title="タイトル4"><img src="./images/photo13.jpg" alt="写真13"></a></li>
-            <li><a href="./images/photo14_lg.jpg" data-size="960x640" data-title="タイトル5"><img src="./images/photo14.jpg" alt="写真14"></a></li>
-            <li><a href="./images/photo15_lg.jpg" data-size="960x640" data-title="タイトル6"><img src="./images/photo15.jpg" alt="写真15"></a></li>
+            <li><a href="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo10_lg.jpg" data-size="960x640" data-title="1"><img src="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo10.jpg" alt="10"></a></li>
+            <li><a href="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo11_lg.jpg" data-size="960x640" data-title="2"><img src="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo11.jpg" alt="11"></a></li>
+            <li><a href="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo12_lg.jpg" data-size="960x640" data-title="3"><img src="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo12.jpg" alt="12"></a></li>
+            <li><a href="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo13_lg.jpg" data-size="960x640" data-title="4"><img src="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo13.jpg" alt="13"></a></li>
+            <li><a href="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo14_lg.jpg" data-size="960x640" data-title="5"><img src="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo14.jpg" alt="14"></a></li>
+            <li><a href="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo15_lg.jpg" data-size="960x640" data-title="6"><img src="http://webdesign-dackel.com/demo/owlcarousel-photoswipe/images/photo15.jpg" alt="15"></a></li>
         </ul>
     </div>
 
