d9cd1a4147b8b046fb3bddb907be81a3a308f0e4
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-05 00:06:22 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -811,6 +811,8 @@ p.article-texterson:first-letter{color:#E63C4D;font-size:1.7em;text-shadow:1px 1
 <script src="{{url('https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.3.0/min/dropzone.min.js')}}"></script>
 
 <script src="{{url('/js/bootstrap.min.js')}}"></script>
+<script src="{{url('/js/jquery.cycle.all.js')}}"></script>
+
 
 <script src="{{url('/js/index.js')}}"></script>
 <script src="{{url('/js/photoswipe.min.js')}}"></script>
