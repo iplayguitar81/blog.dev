ac65d2110d983cb19cdcecca54becfe523e900ad
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-05 13:50:47 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/test_slides.blade.php
+++ resources/views/posts/test_slides.blade.php
@@ -191,5 +191,15 @@
         });
 
     </script>
+    <script src="{{url('/js/jquery.js')}}"></script>
+
+    <script src="{{url('/js/index.js')}}"></script>
+    <script src="{{url('/js/photoswipe.min.js')}}"></script>
+    <script src="{{url('/js/photoswipe-ui-default.min.js')}}"></script>
+
+    {{--getting this part right........ among files to remove after figuring out right gallery sitch:--}}
+
+
+    <script src="{{url('/js/owl.carousel.js')}}"></script>
 
     @endsection
