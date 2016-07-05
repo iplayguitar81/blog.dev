d57e758072bf504302723493990c9d4a6cebcf15
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-04 20:22:57 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/test_code.blade.php
+++ resources/views/posts/test_code.blade.php
@@ -144,7 +144,7 @@
     {{--<a class="btn next btn-danger">Next</a>--}}
 {{--</div>--}}
 
-    <div id="owl-demo" class="my-gallery owl-carousel" itemscope itemtype="http://schema.org/ImageGallery">
+    <div id="owl-demo" class="owl-carousel my-gallery" itemscope itemtype="http://schema.org/ImageGallery">
 
     @foreach($post->images as $image)
         {{--*/ @ $pathy =$image->file_path  /*--}}
@@ -154,12 +154,12 @@
         {{--*/ @ $dimensions =$width.'x'.$height  /*--}}
 
     {{--<div class="item" style="">--}}
-        <div class="item" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+        <figure class="item" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
             <a href="{{url($image->file_path)}}" itemprop="contentUrl" data-size="{{$dimensions}}">
             <img class="thumbnail lazyOwl" src="{{url($image->file_path)}}"  alt=""/>
                 </a>
             <figcaption itemprop="caption description">MVC ASP CRUD height: {{$height}} width: {{$width}}</figcaption>
-        </div>
+        </figure>
 
     {{--</div>--}}
     @endforeach
