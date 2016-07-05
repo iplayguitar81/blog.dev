d868623ad620068f84e57ba27a9f425387f950d3
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-05 00:39:35 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/test_code.blade.php
+++ resources/views/posts/test_code.blade.php
@@ -169,7 +169,7 @@
 
         <figure class="item" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
             <a href="{{url($image->file_path)}}" itemprop="contentUrl" data-size="{{$dimensions}}">
-            <img class="thumbnail lazyOwl" src="{{url($image->file_path)}}"  alt=""/>
+            <img class="thumbnail" src="{{url($image->file_path)}}"  alt=""/>
                 </a>
             <figcaption itemprop="caption description">MVC ASP CRUD height: {{$height}} width: {{$width}}</figcaption>
         </figure>
