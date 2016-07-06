fd93fd17fb855255a934fa06f4eede710aab0174
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-06 00:33:21 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -118,7 +118,7 @@
                     <div class="col-md-6"><p>{{$rating->rate_message}}</p></div>
                     <div class="col-md-2 offset-1">
 
-                            <p>{{$rating->rating}}/5 Stars</p>
+                            <p>{{round($rating->rating,2)}}/5 Stars</p>
                             <div class="rating2"><div class="stars"></div><div class="back" style="width:{{$rate_pct_reviewer}}%;"></div></div>
 
                     </div>
