cbeab782b8e1454991971426dd9b2b94329d9564
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-05 20:29:45 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/test_slides.blade.php
+++ resources/views/posts/test_slides.blade.php
@@ -32,6 +32,78 @@
         </ul>
     </div>
 
+
+
+
+
+
+
+    {{--<div class="slider">--}}
+    {{--<div class="my-gallery" itemscope itemtype="http://schema.org/ImageGallery">--}}
+
+    {{--@foreach($post->images as $image)--}}
+
+
+    {{--*/ @ $pathy =$image->file_path  /*--}}
+
+    {{--*/ @ list($width, $height) = getimagesize($pathy) /*--}}
+
+    {{--*/ @ $dimensions =$width.'x'.$height  /*--}}
+
+
+    {{--<figure class="item" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">--}}
+    {{--<a href="{{url($image->file_path)}}" itemprop="contentUrl" data-size="{{$dimensions}}">--}}
+    {{--<img src="{{url($image->file_path)}}"  alt="CRUD MVC ASP" />--}}
+    {{--</a>--}}
+    {{--<figcaption itemprop="caption description">MVC ASP CRUD height: {{$height}} width: {{$width}}</figcaption>--}}
+    {{--</figure>--}}
+
+
+    {{--@endforeach--}}
+
+    {{--</div>--}}
+    {{--</div>--}}
+
+
+
+
+
+    {{--<br/>--}}
+    {{--<hr>--}}
+    {{--<br/>--}}
+
+    {{--<div class="customNavigation">--}}
+    {{--<a class="btn prev btn-danger">Previous</a>--}}
+    {{--<a class="btn next btn-danger">Next</a>--}}
+    {{--</div>--}}
+
+    {{--<div class="my-gallery">--}}
+    {{--<div id="owl-demo" class="owl-carousel" itemscope itemtype="http://schema.org/ImageGallery">--}}
+
+    {{--@foreach($post->images as $image)--}}
+    {{--*/ @ $pathy =$image->file_path  /*--}}
+
+    {{--*/ @ list($width, $height) = getimagesize($pathy) /*--}}
+
+    {{--*/ @ $dimensions =$width.'x'.$height  /*--}}
+    {{--{{$img = Image::make($image->file_path)->resize(300, 200);}}--}}
+
+    {{--<figure class="item" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">--}}
+    {{--<a class="" href="{{url($image->file_path)}}" itemprop="contentUrl" data-size="{{$dimensions}}">--}}
+    {{--<img class="thumbnail slide" src="{{url($image->file_path)}}"  alt=""/>--}}
+    {{--</a>--}}
+    {{--<figcaption itemprop="caption description">MVC ASP CRUD height: {{$height}} width: {{$width}}</figcaption>--}}
+    {{--</figure>--}}
+
+
+    {{--@endforeach--}}
+
+
+    {{--</div>--}}
+
+
+
+
     <script src="{{url('/js/jquery-1.11.3.min.js')}}"></script>
     <script src="{{url('/js/owl.carousel.js')}}"></script>
     <script src="{{url('/js/photoswipe.min.js')}}"></script>
