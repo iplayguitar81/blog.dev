bfbfe2426cf96d3765cf6ba8407aa65061a162ad
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-05 18:58:57 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -942,18 +942,7 @@ p.article-texterson:first-letter{color:#E63C4D;font-size:1.7em;text-shadow:1px 1
     }
 
 
-    /*@media only screen and (max-width: 960px) {*/
-        /*.container {*/
-            /*!*width:auto;*!*/
-            /*!*padding-right:15px;*!*/
-            /*!*padding-left:15px;*!*/
-        /*}*/
 
-        /*.owl-carousel li {*/
-            /*padding-right:1em;*/
-            /*padding-left:1em;*/
-        /*}*/
-    /*}*/
 
 </style>
 
@@ -967,32 +956,6 @@ p.article-texterson:first-letter{color:#E63C4D;font-size:1.7em;text-shadow:1px 1
             slidesToScroll: 1
     });
     });
-
-//    $("#owl-demo").owlCarousel({
-//        items : 5
-//
-//    });
-
-//        $(document).ready(function() {
-
-
-
-//            var owl = $("#owl-demo");
-//
-//            owl.owlCarousel({dotsEach: true, singleItem:true, lazyLoad : true, navigation : true,nestedItemSelector: 'slide',   afterInit : function(elem){
-//                var that = this
-//                that.owlControls.prependTo(elem)
-//            }});
-//
-//            // Custom Navigation Events
-//            $(".next").click(function(){
-//                owl.trigger('owl.next');
-//            })
-//            $(".prev").click(function(){
-//                owl.trigger('owl.prev');
-//            })
-//
-//    });
 
 
 
