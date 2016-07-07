785b51f06a9709a3b392b5d9a2fb4e60fd251a8c
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-07 01:18:06 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -948,54 +948,7 @@ div.article-texterson2 p:first-letter{color:#E63C4D;font-size:1.7em;text-shadow:
 
 {{--</script>--}}
 
-<script>
 
-    $(document).ready(function(){
-
-
-    });
-    var baseUrl = "{{ url('') }}";
-    var handleDropzoneFileUpload ={
-
-        handleError: function(response) {
-            console.log(response);
-        },
-        handleSuccess: function(response){
-           var imageList =$('#gallery-images ul');
-            var imageSrc = baseUrl + '/' + response.file_path;
-$(imageList).append('<li><a href=""><img src="'+imageSrc +'"></a>{{ Form::open(['route' => ['My.route', $image->id], 'method' => 'delete']) }} <button class="btn btn-danger" type="submit">Delete</button></li>{!! Form::close() !!}');
-        }
-    };
-
-    //
-    //<button class="btn btn-danger" type="submit">Delete</button>
-    {{--{!! Form::submit(Auth::user()->name.' - -Delete Image', ['class' => 'btn btn-danger']) !!}--}}
-    {{--@endif--}}
-
-   //
-
-    //
-    Dropzone.options.addImages ={
-
-        maxFilesize: 2,
-
-        acceptedFiles: 'image/*',
-        success: function(file, response) {
-          console.log(file);
-            console.log(response);
-
-            if(file.status =='success'){
-                handleDropzoneFileUpload.handleSuccess(response);
-
-            }
-            else{
-                handleDropzoneFileUpload.handleError(response);
-            }
-        },
-
-    };
-
-</script>
 </body>
 </html>
 
