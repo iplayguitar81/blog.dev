6ba0c6da57fd00cb9c42fcb1f501222dc252fb54
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-07 20:17:04 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -187,6 +187,7 @@
 <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
 
 <script>
-    tinymce.init({ selector:'textarea',plugins: "media" });
+    tinymce.init({ selector:'textarea',plugins: 'media', editor_selector : 'form-control' });
+
 </script>
 
