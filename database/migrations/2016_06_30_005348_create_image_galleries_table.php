0bf0e9289190cc6c9820905d635b8fee1ed7f202
Chris M Snyder <iplayguitar81@yahoo.com>
2016-07-08 01:45:39 -0700

    get win 10 laravel started yeahhhhhh

--- database/migrations/2016_06_30_005348_create_image_galleries_table.php
+++ database/migrations/2016_06_30_005348_create_image_galleries_table.php
@@ -22,6 +22,7 @@ class CreateImageGalleriesTable extends Migration
             $table->string('file_mime');
             $table->string('file_path');
             $table->string('caption');
+            
             $table->integer('created_by');
 
         });
